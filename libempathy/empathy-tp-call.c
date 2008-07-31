51b845bad569432f501e3e0967cd912e9c1cb875
gcorvala <just.the.boss@gmail.com>
2008-07-31 17:24:17 +0200

    more files for libtp-fs integration

--- libempathy/empathy-tp-call.c
+++ libempathy/empathy-tp-call.c
@@ -35,6 +35,7 @@
 #include "empathy-contact-factory.h"
 #include "empathy-tp-group.h"
 #include "empathy-utils.h"
+#include "empathy-tp-audio-stream.h"
 
 #define DEBUG_FLAG EMPATHY_DEBUG_TP
 #include "empathy-debug.h"
@@ -53,6 +54,16 @@ typedef struct
   gboolean is_incoming;
   guint status;
   gboolean stream_engine_running;
+
+  GstElement *pipeline;
+  GstElement *audiotee;
+  
+  GMutex *mutex;
+
+  GstElement *audiosink;
+  GstElement *audioadder;
+  
+  gint audio_sink_use_count;
 
   EmpathyTpCallStream *audio;
   EmpathyTpCallStream *video;
@@ -411,6 +422,378 @@ tp_call_stream_engine_watch_name_owner_cb (TpDBusDaemon *daemon,
 }
 
 static void
+tp_call_handler_result (TfChannel *chan G_GNUC_UNUSED,
+                GError *error,
+                EmpathyTpCall *call)
+{
+  if (error)
+      DEBUG ("Error handler_result: %s", error->message);
+}
+
+static void
+tp_call_channel_closed (TfChannel *chan, EmpathyTpCall *call)
+{
+  gchar *object_path;
+
+  g_object_get (chan, "object-path", &object_path, NULL);
+
+  g_debug ("channel %s (%p) closed", object_path, chan);
+
+  g_object_unref (chan);
+
+  g_free (object_path);
+
+  //check_if_busy (self);
+}
+
+static void
+tp_call_session_invalidated (TfChannel *chan G_GNUC_UNUSED,
+    FsConference  *conference,
+    FsParticipant *participant,
+    EmpathyTpCall *call)
+{
+  EmpathyTpCallPriv *priv = GET_PRIV (call);
+  
+  gst_element_set_locked_state ((GstElement *) conference, TRUE);
+  gst_element_set_state ((GstElement *) conference, GST_STATE_NULL);
+  gst_element_get_state ((GstElement *) conference, NULL, NULL, GST_CLOCK_TIME_NONE);
+
+  gst_bin_remove (GST_BIN (priv->pipeline), (GstElement *) conference);
+}
+
+static void
+tp_call_channel_session_created (TfChannel *chan G_GNUC_UNUSED,
+    FsConference  *conference,
+    FsParticipant *participant,
+    EmpathyTpCall *call)
+{
+  EmpathyTpCallPriv *priv = GET_PRIV (call);
+  
+  g_debug ("session-created cb");
+  
+  g_object_set (conference, "latency", 100, NULL);
+
+  if (!gst_bin_add (GST_BIN (priv->pipeline), (GstElement *) conference))
+    g_error ("Could not add conference to pipeline");
+
+  gst_element_set_state ((GstElement *) conference, GST_STATE_PLAYING);
+
+  g_signal_connect (priv->channel, "session-invalidated", G_CALLBACK (tp_call_session_invalidated),
+      call);
+}
+
+static GstElement *
+_make_audio_sink (void)
+{
+  const gchar *elem;
+  GstElement *bin = NULL;
+  GstElement *sink = NULL;
+  GstElement *audioconvert = NULL;
+  GstElement *audioresample = NULL;
+  GstPad *pad;
+
+  if ((elem = getenv ("FS_AUDIO_SINK")) || (elem = getenv("FS_AUDIOSINK")))
+    {
+      g_debug ("making audio sink with pipeline \"%s\"", elem);
+      sink = gst_parse_bin_from_description (elem, TRUE, NULL);
+      g_assert (sink);
+    }
+  else
+    {
+#ifdef MAEMO_OSSO_SUPPORT
+      sink = gst_element_factory_make ("dsppcmsink", NULL);
+#else
+      sink = gst_element_factory_make ("gconfaudiosink", NULL);
+
+      if (sink == NULL)
+        sink = gst_element_factory_make ("autoaudiosink", NULL);
+
+      if (sink == NULL)
+        sink = gst_element_factory_make ("alsasink", NULL);
+#endif
+    }
+
+  if (sink == NULL)
+    {
+      g_warning ("failed to make audio sink element!");
+      return NULL;
+    }
+
+  g_debug ("made audio sink element %s", GST_ELEMENT_NAME (sink));
+
+  bin = gst_bin_new ("audiosinkbin");
+
+  if (!gst_bin_add (GST_BIN (bin), sink))
+    {
+      gst_object_unref (bin);
+      gst_object_unref (sink);
+      g_warning ("Could not add sink to bin");
+      return NULL;
+    }
+
+  audioresample = gst_element_factory_make ("audioresample", NULL);
+  if (!gst_bin_add (GST_BIN (bin), audioresample))
+    {
+      gst_object_unref (audioresample);
+      gst_object_unref (bin);
+      g_warning ("Could not add audioresample to the bin");
+      return NULL;
+    }
+
+  audioconvert = gst_element_factory_make ("audioconvert", NULL);
+  if (!gst_bin_add (GST_BIN (bin), audioconvert))
+    {
+      gst_object_unref (audioconvert);
+      gst_object_unref (bin);
+      g_warning ("Could not add audioconvert to the bin");
+      return NULL;
+    }
+
+  if (!gst_element_link_many (audioresample, audioconvert, sink, NULL))
+    {
+      gst_object_unref (bin);
+      g_warning ("Could not link sink elements");
+      return NULL;
+    }
+
+  pad = gst_bin_find_unconnected_pad (GST_BIN (bin), GST_PAD_SINK);
+
+  if (!pad)
+    {
+      gst_object_unref (bin);
+      g_warning ("Could not find unconnected sink pad in src bin");
+      return NULL;
+    }
+
+  if (!gst_element_add_pad (bin, gst_ghost_pad_new ("sink", pad)))
+    {
+      gst_object_unref (bin);
+      g_warning ("Could not add pad to bin");
+      return NULL;
+    }
+
+  gst_object_unref (pad);
+
+
+  return bin;
+}
+
+static gboolean
+tp_call_add_audio_sink_locked (EmpathyTpCall *call)
+{
+  GstElement *sink = NULL;
+  GstElement *liveadder = NULL;
+  EmpathyTpCallPriv *priv = GET_PRIV (call);
+
+  sink = _make_audio_sink ();
+
+  if (!sink)
+    {
+      g_warning ("Could not make audio sink");
+      goto error;
+    }
+
+  if (!gst_bin_add (GST_BIN (priv->pipeline), sink))
+    {
+      g_warning ("Could not add audio sink to pipeline");
+      gst_object_unref (sink);
+      sink = NULL;
+      goto error;
+    }
+
+  liveadder = gst_element_factory_make ("liveadder", NULL);
+  if (!liveadder)
+    {
+      g_warning ("Could not create liveadder");
+      goto error;
+    }
+
+  if (!gst_bin_add (GST_BIN (priv->pipeline), liveadder))
+    {
+      gst_object_unref (liveadder);
+      liveadder = NULL;
+      g_warning ("Could not add liveadder to the bin");
+      goto error;
+    }
+
+  if (!gst_element_link_pads (liveadder, "src", sink, "sink"))
+    {
+      g_warning ("Could not linnk the liveadder to the sink");
+      goto error;
+    }
+
+  if (gst_element_set_state (sink, GST_STATE_PLAYING) ==
+      GST_STATE_CHANGE_FAILURE ||
+      gst_element_set_state (liveadder, GST_STATE_PLAYING) ==
+      GST_STATE_CHANGE_FAILURE)
+    {
+      g_warning ("Could not start sink or liveadder");
+      goto error;
+    }
+
+  priv->audiosink = sink;
+  priv->audioadder = liveadder;
+
+  return TRUE;
+
+ error:
+  if (sink)
+    {
+      gst_element_set_locked_state (sink, TRUE);
+      gst_element_set_state (sink, GST_STATE_NULL);
+      gst_bin_remove (GST_BIN (priv->pipeline), sink);
+    }
+  if (liveadder)
+    {
+      gst_element_set_locked_state (liveadder, TRUE);
+      gst_element_set_state (liveadder, GST_STATE_NULL);
+      gst_bin_remove (GST_BIN (priv->pipeline), liveadder);
+    }
+
+  return FALSE;
+}
+
+static GstPad *
+tp_call_audio_stream_request_pad (EmpathyTpAudioStream *audiostream,
+    EmpathyTpCall *call)
+{
+  EmpathyTpCallPriv *priv = GET_PRIV (call);
+  GstPad *pad = NULL;
+
+  g_mutex_lock (priv->mutex);
+
+  if (priv->audiosink == NULL)
+    if (!tp_call_add_audio_sink_locked (call))
+      goto out;
+
+  g_assert (priv->audioadder);
+
+  pad = gst_element_get_request_pad (priv->audioadder, "sink%d");
+
+  priv->audio_sink_use_count++;
+
+ out:
+  g_mutex_unlock (priv->mutex);
+
+  return pad;
+}
+
+static void
+tp_call_remove_audio_sink_locked (EmpathyTpCall *call)
+{
+  EmpathyTpCallPriv *priv = GET_PRIV (call);
+  
+  gst_element_set_locked_state (priv->audiosink, TRUE);
+  gst_element_set_locked_state (priv->audioadder, TRUE);
+  gst_element_set_state (priv->audiosink, GST_STATE_NULL);
+  gst_element_set_state (priv->audioadder, GST_STATE_NULL);
+
+  gst_bin_remove (GST_BIN (priv->pipeline), priv->audioadder);
+  gst_bin_remove (GST_BIN (priv->pipeline), priv->audiosink);
+
+  priv->audiosink = NULL;
+  priv->audioadder = NULL;
+}
+
+static void
+tp_call_audio_stream_release_pad (EmpathyTpAudioStream *audiostream,
+    GstPad *pad,
+    EmpathyTpCall *call)
+{
+  EmpathyTpCallPriv *priv = GET_PRIV (call);
+
+  g_mutex_lock (priv->mutex);
+
+  gst_element_release_request_pad (priv->audioadder, pad);
+
+  priv->audio_sink_use_count--;
+
+  if (priv->audio_sink_use_count <= 0)
+    {
+      priv->audio_sink_use_count = 0;
+
+      tp_call_remove_audio_sink_locked (call);
+    }
+
+  g_mutex_unlock (priv->mutex);
+}
+
+static void
+tp_call_channel_stream_created (TfChannel *chan G_GNUC_UNUSED,
+    TfStream *stream, EmpathyTpCall *call)
+{
+  guint media_type;
+  GError *error = NULL;
+  EmpathyTpCallPriv *priv = GET_PRIV (call);
+
+  g_object_get (G_OBJECT (stream), "media-type", &media_type, NULL);
+
+  if (media_type == TP_MEDIA_STREAM_TYPE_AUDIO)
+    {
+      EmpathyTpAudioStream *audiostream;
+      GstPad *pad;
+
+      pad = gst_element_get_request_pad (priv->audiotee, "src%d");
+
+      audiostream = empathy_tp_audio_stream_new (stream,
+          GST_BIN (priv->pipeline), pad, &error);
+
+      if (!audiostream)
+        {
+          g_warning ("Could not create audio stream: %s", error->message);
+          gst_element_release_request_pad (priv->audiotee, pad);
+          return;
+        }
+      g_clear_error (&error);
+      g_object_set_data ((GObject*) stream, "se-stream", audiostream);
+
+      g_signal_connect (audiostream,
+          "request-pad",
+          G_CALLBACK (tp_call_audio_stream_request_pad),
+          call);
+      g_signal_connect (audiostream,
+          "release-pad",
+          G_CALLBACK (tp_call_audio_stream_release_pad),
+          call);
+
+    }
+  else if (media_type == TP_MEDIA_STREAM_TYPE_VIDEO)
+    {
+      //TpStreamEngineVideoStream *videostream = NULL;
+      //GstPad *pad;
+
+      //pad = gst_element_get_request_pad (self->priv->videotee, "src%d");
+
+      //videostream = tp_stream_engine_video_stream_new (stream,
+          //GST_BIN (self->priv->pipeline), pad, &error);
+
+      //if (!videostream)
+        //{
+          //g_warning ("Could not create video stream: %s", error->message);
+          //gst_element_release_request_pad (self->priv->videotee, pad);
+          //return;
+        //}
+      //g_clear_error (&error);
+      //g_object_set_data ((GObject*) stream, "se-stream", videostream);
+
+      //g_mutex_lock (self->priv->mutex);
+      //self->priv->output_sinks = g_list_prepend (self->priv->output_sinks,
+          //videostream);
+      //g_mutex_unlock (self->priv->mutex);
+
+      //g_signal_connect (videostream, "receiving",
+          //G_CALLBACK (stream_receiving), self);
+    }
+
+  //g_signal_connect (stream, "closed", G_CALLBACK (stream_closed), self);
+
+  //g_signal_connect (stream, "request-resource",
+      //G_CALLBACK (stream_request_resource), self);
+  //g_signal_connect (stream, "free-resource",
+      //G_CALLBACK (stream_free_resource), self);
+}
+
+static void
 tp_call_stream_engine_handle_channel (EmpathyTpCall *call)
 {
   EmpathyTpCallPriv *priv = GET_PRIV (call);
@@ -420,28 +803,7 @@ tp_call_stream_engine_handle_channel (EmpathyTpCall *call)
   guint handle_type;
   guint handle;
   TpProxy *connection;
-
-  DEBUG ("Revving up the stream engine");
-
-  priv->stream_engine = g_object_new (TP_TYPE_PROXY,
-      "bus-name", STREAM_ENGINE_BUS_NAME,
-      "dbus-connection", tp_get_bus (),
-      "object-path", STREAM_ENGINE_OBJECT_PATH,
-       NULL);
-  tp_proxy_add_interface_by_id (priv->stream_engine,
-      EMP_IFACE_QUARK_STREAM_ENGINE);
-  tp_proxy_add_interface_by_id (priv->stream_engine,
-      EMP_IFACE_QUARK_CHANNEL_HANDLER);
-
-  g_signal_connect (priv->stream_engine, "invalidated",
-      G_CALLBACK (tp_call_stream_engine_invalidated_cb),
-      call);
-  
-  /* FIXME: dbus daemon should be unique */
-  priv->dbus_daemon = tp_dbus_daemon_new (tp_get_bus ());
-  tp_dbus_daemon_watch_name_owner (priv->dbus_daemon, STREAM_ENGINE_BUS_NAME,
-      tp_call_stream_engine_watch_name_owner_cb,
-      call, NULL);
+  GError *error = NULL;
 
   g_object_get (priv->channel,
       "channel", &chan,
@@ -455,12 +817,47 @@ tp_call_stream_engine_handle_channel (EmpathyTpCall *call)
       "handle", &handle,
       NULL);
 
-  emp_cli_channel_handler_call_handle_channel (priv->stream_engine, -1,
-        connection->bus_name,
-        connection->object_path,
-        channel_type, object_path, handle_type, handle,
-        tp_call_async_cb, "calling handle channel", NULL,
-        G_OBJECT (call));
+  //-------------------------------------------------------------------
+  g_debug("HandleChannel called");
+
+  if (strcmp (channel_type, TP_IFACE_CHANNEL_TYPE_STREAMED_MEDIA) != 0)
+    {
+      GError e = { TP_ERRORS, TP_ERROR_INVALID_ARGUMENT,
+        "TpCall was passed a channel that was not a "
+        TP_IFACE_CHANNEL_TYPE_STREAMED_MEDIA };
+
+      g_message ("%s", e.message);
+      return;
+     }
+
+  if (priv->channel == NULL)
+    {
+      g_error_free (error);
+      g_debug ("TfChannel == NULL");
+      return;
+    }
+
+  g_signal_connect (priv->channel,
+      "handler-result",
+      G_CALLBACK (tp_call_handler_result),
+      call);
+  g_signal_connect (priv->channel,
+      "closed",
+      G_CALLBACK (tp_call_channel_closed),
+      call);
+  g_signal_connect (priv->channel,
+      "session-created",
+      G_CALLBACK (tp_call_channel_session_created),
+      call);
+  g_signal_connect (priv->channel,
+      "stream-created",
+      G_CALLBACK (tp_call_channel_stream_created),
+      call);
+  //g_signal_connect (chan, "stream-get-codec-config",
+      //G_CALLBACK (stream_get_codec_config), self);
+
+  //g_signal_emit (self, signals[HANDLING_CHANNEL], 0);
+  //-------------------------------------------------------------------
 
   g_object_unref (connection);
   g_free (channel_type);
@@ -488,8 +885,10 @@ tp_call_constructor (GType type,
       NULL);
 
   /* Setup streamed media channel */
-  g_signal_connect (chan, "invalidated",
-      G_CALLBACK (tp_call_channel_invalidated_cb), call);
+  g_signal_connect (chan,
+      "invalidated",
+      G_CALLBACK (tp_call_channel_invalidated_cb),
+      call);
   tp_cli_channel_type_streamed_media_connect_to_stream_added (chan,
       tp_call_stream_added_cb, NULL, NULL, G_OBJECT (call), NULL);
   tp_cli_channel_type_streamed_media_connect_to_stream_removed (chan,
@@ -741,34 +1140,34 @@ void
 empathy_tp_call_add_preview_video (EmpathyTpCall *call,
                                    guint preview_video_socket_id)
 {
-  EmpathyTpCallPriv *priv = GET_PRIV (call);
+  //EmpathyTpCallPriv *priv = GET_PRIV (call);
 
   g_return_if_fail (EMPATHY_IS_TP_CALL (call));
 
   DEBUG ("Adding preview video");
 
-  emp_cli_stream_engine_call_add_preview_window (priv->stream_engine, -1,
-      preview_video_socket_id,
-      tp_call_async_cb,
-      "adding preview window", NULL,
-      G_OBJECT (call));
+  //emp_cli_stream_engine_call_add_preview_window (priv->stream_engine, -1,
+      //preview_video_socket_id,
+      //tp_call_async_cb,
+      //"adding preview window", NULL,
+      //G_OBJECT (call));
 }
 
 void
 empathy_tp_call_remove_preview_video (EmpathyTpCall *call,
                                       guint preview_video_socket_id)
 {
-  EmpathyTpCallPriv *priv = GET_PRIV (call);
+  //EmpathyTpCallPriv *priv = GET_PRIV (call);
 
   g_return_if_fail (EMPATHY_IS_TP_CALL (call));
 
   DEBUG ("Removing preview video");
 
-  emp_cli_stream_engine_call_remove_preview_window (priv->stream_engine, -1,
-      preview_video_socket_id,
-      tp_call_async_cb,
-      "removing preview window", NULL,
-      G_OBJECT (call));
+  //emp_cli_stream_engine_call_remove_preview_window (priv->stream_engine, -1,
+      //preview_video_socket_id,
+      //tp_call_async_cb,
+      //"removing preview window", NULL,
+      //G_OBJECT (call));
 }
 
 void
